afbc5f29bc27e66dd7804c8c8d550839d6a3edf4
arpanpathak <arpanpathak1996@gmail.com>
2018-03-30 20:10:29 +0530

    local commit

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -12,4 +12,17 @@ module.exports = {
   },
   hashSecret1: 'thisIsASecret',
   hashSecret2: 'thisIsAlsoASecret',
+  api: {
+  	googleMap: {
+  		"key": "AIzaSyDv084m8ZnoPEnwZSEApjfVwM0eCY6OZ6M",
+  	},
+  	googleOAuth: {
+  		"client_id": 					"250199819534-gof813kaa3ni165v67t455oqadtt1dmi.apps.googleusercontent.com",
+  		"project_id": 					"addernet-199612",
+  		"auth_uri": 					"https://accounts.google.com/o/oauth2/auth",
+  		"token_uri": 					"https://accounts.google.com/o/oauth2/token",
+  		"auth_provider_x509_cert_url":  "https://www.googleapis.com/oauth2/v1/certs",
+  		"client_secret": 				"ETf36FQMkQNiXMf2j_goZqio"
+  	} 	
+  }
 };
